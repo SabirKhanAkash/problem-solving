fa650c3bac6e025331f16ab02e782a05d957c4cf
Sabir Khan Akash <4175.akash@gmail.com>
2022-02-14 15:03:00 +0600

    from CF Global Round 19

--- CodeForces/1637C. Andrew and Stones.cpp
+++ CodeForces/1637C. Andrew and Stones.cpp
@@ -27,31 +27,19 @@ using namespace std;
 #define arrbsort(a)            sort(a, a + (sizeof(a) / sizeof(a[0])), greater<ll>())
 #define i2s(arr,a)             std::sprintf(arr, "%d", a);
 
-int sum = 0;
+ll sum = 0,o=0;
 
 bool isAllEven(ll arr[],ll n)
 {
+    sum = 0;
     ll i,e=0;
     loop(i,n)
-    {
         if((arr[i])%2== 0) { e++; sum += arr[i]; }
 
-        if(e == n) return true;
-        else return false;
-    }
+    if(e == n) return true;
+    else return false;
 }
 
-bool isAllOdd(ll arr[],ll n)
-{
-    ll i,o=0;
-    loop(i,n)
-    {
-        if(arr[i] %2 != 0) o++;
-
-        if(o == n) return true;
-        else return false;
-    }
-}
 
 int main()
 {
@@ -60,27 +48,42 @@ int main()
 
     tc(t)
     {
-        ll i,n,arr[100000],midarr[100000],k=0,p;
+        ll i,n,arr[100000],midarr[100000],k=0,p,c_o=0,c_One=0;
         sl(n);
         loop(i,n)
         {
             sl(arr[i]);
-            if(i!=0 || i!=(n-1))
+
+            if(i!=0 && i!=(n-1))
             {
                 midarr[k] = arr[i];
+                if(arr[i] == 1) c_One++;
                 k++;
             }
 
         }
 
+
         p = n-2;
 
-
-        if(isAllOdd(midarr,p) == true)
+        if(c_One == p || ((p == 1)&&(midarr[0]%2!=0)))
             cout<<"-1"<<endl;
-        else if(isAllEven(midarr,p) == true)
-            cout<<(sum/2)<<endl;
+        else
+        {
+            loop(i,p)
+            {
+                if(midarr[i] % 2 != 0)
+                {
+                    c_o++;
+                    midarr[i] = midarr[i] + 1;
+                }
+            }
+            if(isAllEven(midarr,p) == true)
+                cout<<(sum/2)<<endl;
+        }
+
     }
 
     return 0;
 }
+
