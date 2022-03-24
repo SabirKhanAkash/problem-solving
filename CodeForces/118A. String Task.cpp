8ad102d2a9210b511148420bbf79907a56842685
Sabir Khan Akash <4175.akash@gmail.com>
2022-03-25 02:41:17 +0600

    From CF Beta Round #89 (Div. 2)

---
+++ CodeForces/118A. String Task.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+//<--------- Coded by SabirKhanAkash 1603108 --------->
+
+#define ll                     long long
+#define p(a)                   cout<<a<<" "
+#define pd(a)                  printf("%lf",a)
+#define pf(a)                   printf("%f",a)
+#define sd(a)                  scanf("%lf",&a)
+#define sf(a)                  scanf("%f",&a)
+#define si(a)                  scanf("%d",&a)
+#define sl(a)                  scanf("%lld",&a)
+#define loop(i,b)              for(i=0;i<b;i++)
+#define loope(i,b)             for(i=0;i<=b;i++)
+#define FOR(i,a,b)             for(i=a;i<b;i++)
+#define FORE(i,a,b)            for(i=a;i<=b;i++)
+#define rev(s)                 reverse(s.begin(),s.end())
+#define inf                    9000000000000000
+#define pie                    3.1415926536
+#define pb(a)                  push_back(a)
+#define pop(a)                pop_back(a)
+#define in(a,b)                insert({a,b})
+#define test                   printf(" <---I'm Here--> ")
+#define tc(a)                  while(a--)
+#define nl                     printf("\n")
+#define sorted(a)              sort(a.begin(),a.end())
+#define bsorted(a)             sort(a.begin(),a.end(),greater<ll>())
+#define arrsort(a)             sort(a, a + sizeof(a) / sizeof(a[0]))
+#define arrbsort(a)            sort(a, a + (sizeof(a) / sizeof(a[0])), greater<ll>())
+#define i2s(arr,a)             std::sprintf(arr, "%d", a);
+
+int main()
+{
+    int i;
+    string s;
+    cin>>s;
+    vector<char> letter(s.begin(), s.end());
+    vector<char> Output;
+    for (const char &c: letter) {
+        if(c =='A'|| c =='I' || c =='E' || c =='O' || c =='U' || c  =='Y' || c =='a'|| c =='i' || c =='e' || c =='o' || c =='u' || c =='y') 
+            cout<<"";
+        else
+        {
+        	Output.pb('.');
+            Output.pb(tolower(c));
+        }
+    }
+
+    for(vector<char>::iterator it=Output.begin(); it!=Output.end() ; it++)
+        cout<<*it;
+
+    return 0;
+}
