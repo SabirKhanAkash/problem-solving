963465864f273ee139c38595acd4fafc24fb2d79
Sabir Khan Akash <4175.akash@gmail.com>
2022-03-30 03:04:02 +0600

    file removed

--- CodeForces/236A. Boy or Girl.cpp
+++ CodeForces/236A. Boy or Girl.cpp
@@ -1,70 +1,60 @@
-#define ll long long
-#define p() printf("I'm Here Now");
-
-#define pi(x) printf("%d",x)
-#define pf(x) printf("%f",x)
-#define pd(x) printf("%lf",x)
-#define pc(x) printf("%c",x)
-#define ps(x) printf("%s",x)
-
-#define si(x) scanf("%d",&x)
-#define sf(x) scanf("%f",&x)
-#define sd(x) scanf("%lf",&x)
-#define sc(x) scanf("%c",&x)
-#define ss(x) scanf("%s",&x)
-
-#define nl() printf("\n");
-#define psp() printf(" ")
-
-#define loop(i,p,q,r) for(int i=p; i<q; i=i+r)
-
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<conio.h>
-#include<string.h>
-#include<math.h>
-#include<algorithm>
-#include <iostream>
-#include<vector>
-#include<map>
-#include<unordered_map>
-//#include<bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-
-void findDistinct(string userName)
-{
-    unordered_map<char, int> d;
-    for(char i: userName)
-        d[i] = d[i] +1;
-
-    for(char i:userName)
-    {
-        if(d[i]!=0)
-        {
-            pc(i);
-            psp();
-            pi(d[i]);
-            nl();
-            d[i] = 0;
-
-        }
-
-    }
-
-    nl();
-}
-
+//<--------- Coded by SabirKhanAkash 1603108 --------->
+ 
+#define ll                     long long
+#define p(a)                   cout<<a<<" "
+#define pd(a)                  printf("%lf",a)
+#define pf(a)                   printf("%f",a)
+#define sd(a)                  scanf("%lf",&a)
+#define sf(a)                  scanf("%f",&a)
+#define si(a)                  scanf("%d",&a)
+#define sl(a)                  scanf("%lld",&a)
+#define loop(i,b)              for(i=0;i<b;i++)
+#define loope(i,b)             for(i=0;i<=b;i++)
+#define FOR(i,a,b)             for(i=a;i<b;i++)
+#define FORE(i,a,b)            for(i=a;i<=b;i++)
+#define rev(s)                 reverse(s.begin(),s.end())
+#define inf                    9000000000000000
+#define pie                    3.1415926536
+#define pb(a)                  push_back(a)
+#define pop(a)                pop_back(a)
+#define in(a,b)                insert({a,b})
+#define test                   printf(" <---I'm Here--> ")
+#define tc(a)                  while(a--)
+#define nl                     printf("\n")
+#define sorted(a)              sort(a.begin(),a.end())
+#define bsorted(a)             sort(a.begin(),a.end(),greater<ll>())
+#define arrsort(a)             sort(a, a + sizeof(a) / sizeof(a[0]))
+#define arrbsort(a)            sort(a, a + (sizeof(a) / sizeof(a[0])), greater<ll>())
+#define i2s(arr,a)             std::sprintf(arr, "%d", a);
+ 
 int main()
 {
-    string userName;
-    ss(userName);
-
-    findDistinct(userName);
-
-
-
+	int count=0;
+    string str;
+    cin>>str;
+    
+    unordered_map<char, int> M;
+ 
+    for (int i = 0; str[i]; i++)
+    {
+        if (M.find(str[i]) == M.end())
+            M.insert(make_pair(str[i], 1));
+ 
+        else
+            M[str[i]]++;
+    }
+ 
+    for (auto& it : M) {
+    	count++;
+        //cout << count<<" "<<it.first << ' ' << it.second << '\n';
+    }
+    
+    if(count%2!=0)
+    	cout<<"IGNORE HIM!"<<endl;
+    else
+    	cout<<"CHAT WITH HER!"<<endl;
+ 
     return 0;
 }
