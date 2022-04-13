2fa0084638f8f47792574032e60cc04ec7d9f75c
Sabir Khan Akash <4175.akash@gmail.com>
2022-04-13 06:46:00 +0600

    from CF Round #479 (Div. 3)

---
+++ CodeForces/977A. Wrong Subtraction.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+//<--------- Coded by SabirKhanAkash 1603108 --------->
+
+#define ll                     long long
+#define p(a)                   cout<<a<<" "
+#define pd(a)                  printf("%lf",a)
+#define pf(a)                   printf("%f",a)
+#define sd(a)                  scanf("%lf",&a)
+#define sf(a)                  scanf("%f",&a)
+#define si(a)                  scanf("%d",&a)
+#define sl(a)                  scanf("%lld",&a)
+#define loop(i,b)              for(i=0;i<b;i++)
+#define loope(i,b)             for(i=0;i<=b;i++)
+#define FOR(i,a,b)             for(i=a;i<b;i++)
+#define FORE(i,a,b)            for(i=a;i<=b;i++)
+#define rev(s)                 reverse(s.begin(),s.end())
+#define inf                    9000000000000000
+#define pie                    3.1415926536
+#define pb(a)                  push_back(a)
+#define pop(a)                pop_back(a)
+#define in(a,b)                insert({a,b})
+#define test                   printf(" <---I'm Here--> ")
+#define tc(a)                  while(a--)
+#define nl                     printf("\n")
+#define sorted(a)              sort(a.begin(),a.end())
+#define bsorted(a)             sort(a.begin(),a.end(),greater<ll>())
+#define arrsort(a)             sort(a, a + sizeof(a) / sizeof(a[0]))
+#define arrbsort(a)            sort(a, a + (sizeof(a) / sizeof(a[0])), greater<ll>())
+#define i2s(arr,a)             std::sprintf(arr, "%d", a);
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	ll n,x;
+	int k,i;
+	
+	cin>>n;
+	si(k);
+	
+	loop(i,k)
+	{
+		x = n % 10;
+		if(x!=0)
+			n--;
+		else
+			n/=10;
+	}
+	cout<<n<<endl;
+	
+	
+	
+	return 0;
+}
