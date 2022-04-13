cd6a7629849966125c36891e30ff2e8e190d9753
Sabir Khan Akash <4175.akash@gmail.com>
2022-04-13 06:45:40 +0600

    from School Team Contest #2 (Winter Computer School 2010/11)

--- CodeForces/44A. Indian Summer.cpp
+++ CodeForces/44A. Indian Summer.cpp
@@ -1,29 +1,64 @@
-#include<iostream>
-#include<string.h>
+#include<bits/stdc++.h>
 using namespace std;
+//<--------- Coded by SabirKhanAkash 1603108 --------->
+
+#define ll                     long long
+#define p(a)                   cout<<a<<" "
+#define pd(a)                  printf("%lf",a)
+#define pf(a)                   printf("%f",a)
+#define sd(a)                  scanf("%lf",&a)
+#define sf(a)                  scanf("%f",&a)
+#define si(a)                  scanf("%d",&a)
+#define sl(a)                  scanf("%lld",&a)
+#define loop(i,b)              for(i=0;i<b;i++)
+#define loope(i,b)             for(i=0;i<=b;i++)
+#define FOR(i,a,b)             for(i=a;i<b;i++)
+#define FORE(i,a,b)            for(i=a;i<=b;i++)
+#define rev(s)                 reverse(s.begin(),s.end())
+#define inf                    9000000000000000
+#define pie                    3.1415926536
+#define pb(a)                  push_back(a)
+#define pop(a)                pop_back(a)
+#define in(a,b)                insert({a,b})
+#define test                   printf(" <---I'm Here--> ")
+#define tc(a)                  while(a--)
+#define nl                     printf("\n")
+#define sorted(a)              sort(a.begin(),a.end())
+#define bsorted(a)             sort(a.begin(),a.end(),greater<ll>())
+#define arrsort(a)             sort(a, a + sizeof(a) / sizeof(a[0]))
+#define arrbsort(a)            sort(a, a + (sizeof(a) / sizeof(a[0])), greater<int>())
+#define i2s(arr,a)             std::sprintf(arr, "%d", a);
+
 int main()
 {
-    string leave_name;
-    string leave_color;
-    string new_string;
-    string temp[2010];
-    int n,i,j,count=0;
-    cin>>n;
-    for (i=0 ; i<n ; i++) {
-        cin>>leave_name>>leave_color;
-        new_string = leave_name + leave_color;
+    int i,n,dup_count=0;
+    si(n);
+    string species[100],color[100],newstr[200];
 
+    loop(i,n)
+        cin>>species[i]>>color[i];
 
-        temp[i] = new_string;
-        for (j=0 ; j<i && i>0 ; j++) {
-            if (new_string == temp[j] ) {
-                count--;
-                cout<<count<<endl;
-            }
-        }
-        count++;
-        cout<<count<<endl;
+    loop(i,n)
+    {
+        newstr[i] = species[i] +"+"+ color[i];
     }
-    cout<<count<<endl;
+
+    loop(i,n)
+    {
+        for(int j=i+1; j<n ; j++)
+        {
+            if(newstr[i] == newstr[j])
+            {
+                dup_count++;
+                break;
+            }
+                
+        }
+    }
+    if(n-dup_count == 0)
+        cout<<"1"<<endl;
+    else
+        cout<<n-dup_count<<endl;
+
     return 0;
 }
