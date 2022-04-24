f70974016646ec17a9c36aa2ad95bb583d6091cf
Sabir Khan Akash <4175.akash@gmail.com>
2022-04-25 02:21:35 +0600

    from cf

---
+++ CodeForces/112A. Petya and Strings.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#include <ctype.h>
+using namespace std;
+//<--------- Coded by SabirKhanAkash 1603108 --------->
+
+#define ll                     long long
+#define p(a)                   cout<<a<<" "
+#define pd(a)                  printf("%lf",a)
+#define pf(a)                   printf("%f",a)
+#define sd(a)                  scanf("%lf",&a)
+#define sf(a)                  scanf("%f",&a)
+#define si(a)                  scanf("%d",&a)
+#define sl(a)                  scanf("%lld",&a)
+#define sca(a)                 scanf("%s",a)
+#define loop(i,b)              for(i=0;i<b;i++)
+#define loope(i,b)             for(i=0;i<=b;i++)
+#define FOR(i,a,b)             for(i=a;i<b;i++)
+#define FORE(i,a,b)            for(i=a;i<=b;i++)
+#define rev(s)                 reverse(s.begin(),s.end())
+#define inf                    9000000000000000
+#define pie                    3.1415926536
+#define pb(a)                  push_back(a)
+#define pop(a)                pop_back(a)
+#define in(a,b)                insert({a,b})
+#define test                   printf(" <---I'm Here--> ")
+#define tc(a)                  while(a--)
+#define nl                     printf("\n")
+#define sorted(a)              sort(a.begin(),a.end())
+#define bsorted(a)             sort(a.begin(),a.end(),greater<ll>())
+#define arrsort(a)             sort(a, a + sizeof(a) / sizeof(a[0]))
+#define arrbsort(a)            sort(a, a + (sizeof(a) / sizeof(a[0])), greater<ll>())
+#define i2s(arr,a)             std::sprintf(arr, "%d", a);
+
+int main()
+{
+	int n,m,i,sum_a=0,sum_b=0;
+    string a,b;
+    cin>>a>>b;
+    
+    transform(a.begin(), a.end(), a.begin(), ::tolower);
+    transform(b.begin(), b.end(), b.begin(), ::tolower);
+
+    // cout<<(a)<<endl;
+    // cout<<(b)<<endl;
+    
+    if(a.compare(b)==0)
+    	cout<<"0"<<endl;
+    else if(a.compare(b) > 0)
+    	cout<<"1"<<endl;
+    else if(a.compare(b) < 0)
+    	cout<<"-1"<<endl;
+
+    return 0;
+}
