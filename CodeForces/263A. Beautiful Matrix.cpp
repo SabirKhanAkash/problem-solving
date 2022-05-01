575188f7fb2a2ea83d130b7d717eb2de76ff99cf
Sabir Khan Akash <4175.akash@gmail.com>
2022-05-02 02:18:57 +0600

    from cf

---
+++ CodeForces/263A. Beautiful Matrix.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+//<--------- Coded by SabirKhanAkash 1603108 --------->
+
+#define ll                     long long
+#define p(a)                   cout<<a<<" "
+#define pd(a)                  printf("%lf",a)
+#define pf(a)                   printf("%f",a)
+#define sd(a)                  scanf("%lf",&a)
+#define sf(a)                  scanf("%f",&a)
+#define si(a)                  scanf("%d",&a)
+#define sl(a)                  scanf("%lld",&a)
+#define sca(a)                 scanf("%s",a)
+#define loop(i,b)              for(i=0;i<b;i++)
+#define loope(i,b)             for(i=0;i<=b;i++)
+#define FOR(i,a,b)             for(i=a;i<b;i++)
+#define FORE(i,a,b)            for(i=a;i<=b;i++)
+#define rev(s)                 reverse(s.begin(),s.end())
+#define inf                    9000000000000000
+#define eps                    1e-9
+#define pie                    3.1415926536
+#define pb(a)                  push_back(a)
+#define pop(a)                pop_back(a)
+#define in(a,b)                insert({a,b})
+#define test                   printf(" <---I'm Here--> ")
+#define tc(a)                  while(a--)
+#define nl                     printf("\n")
+#define sorted(a)              sort(a.begin(),a.end())
+#define bsorted(a)             sort(a.begin(),a.end(),greater<ll>())
+#define arrsort(a,n)             sort(a, a + n)
+#define arrbsort(a)            sort(a, a + n, greater<ll>())
+#define i2s(arr,a)             std::sprintf(arr, "%d", a);
+#define tolow(a)               transform(a.begin(), a.end(), a.begin(), ::tolower)
+#define toup(a)               transform(a.begin(), a.end(), a.begin(), ::toupper)
+#define power(a,b)             (int)(pow(a,b)+eps)
+
+int main()
+{
+    int i,j,m,n;
+    int mat[6][6];
+    FORE(i,1,5)
+    {
+        FORE(j,1,5)
+        {
+            cin>>(mat[i][j]);
+            if(mat[i][j] == 1)
+            {
+                m = i;
+                n = j;
+            }
+        }
+    }
+    cout<<((abs(3-m))+(abs(3-n)))<<endl;
+
+    return 0;
+}
