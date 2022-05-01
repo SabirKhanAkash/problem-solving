f2cef3d2bdb9306039dcbe6555181d8eb834a374
Sabir Khan Akash <4175.akash@gmail.com>
2022-05-02 03:33:48 +0600

    from FlipCart interview and GeeksForGeeks

---
+++ Others/Chocolate Distribution Problem.cpp
@@ -0,0 +1,72 @@
+// From GeeksForGeeks | Flipcart Interview Question
+
+#include<bits/stdc++.h>
+using namespace std;
+//<--------- Coded by SabirKhanAkash 1603108 --------->
+
+#define ll                     long long
+#define p(a)                   cout<<a<<" "
+#define pd(a)                  printf("%lf",a)
+#define pf(a)                   printf("%f",a)
+#define sd(a)                  scanf("%lf",&a)
+#define sf(a)                  scanf("%f",&a)
+#define si(a)                  scanf("%d",&a)
+#define sl(a)                  scanf("%lld",&a)
+#define loop(i,b)              for(i=0;i<b;i++)
+#define loope(i,b)             for(i=0;i<=b;i++)
+#define FOR(i,a,b)             for(i=a;i<b;i++)
+#define FORE(i,a,b)            for(i=a;i<=b;i++)
+#define rev(s)                 reverse(s.begin(),s.end())
+#define revi(n,r)              r = r*10 + n%10
+#define inf                    9000000000000000
+#define pie                    3.1415926536
+#define pb(a)                  push_back(a)
+#define test                   printf(" <---I'm Here--> ")
+#define tc(a)                  while(a--)
+#define nl                     printf("\n")
+#define sorted(a)              sort(a.begin(),a.end())
+#define bsorted(a)             sort(a.begin(),a.end(),greater<ll>())
+#define arrsort(a)             sort(a, a + sizeof(a) / sizeof(a[0]))
+#define arrbsort(a)            sort(a, a + (sizeof(a) / sizeof(a[0])), greater<ll>())
+#define i2s(arr,a)             std::sprintf(arr, "%d", a);
+
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+        ll i;
+        vector<int>ans;
+        sorted(a);
+        
+        for(i=0; i+m-1<n ; i++)
+            ans.pb(a.at(i+(m-1)) - a.at(i));
+            
+        sorted(ans);
+        return ans.at(0);
+        
+    }   
+};
+
+// { Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
