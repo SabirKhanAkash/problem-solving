c134af24fb1f8778216b74b23eaabcb746e55f5e
Sabir Khan Akash <4175.akash@gmail.com>
2022-05-12 22:25:48 +0600

    from CF Round #787 (Div. 3)

---
+++ CodeForces/1675B. Make It Increasing.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std;
+//<--------- Coded by SabirKhanAkash 1603108 --------->
+
+#define ll                     long long
+#define p(a)                   cout<<a<<" "
+#define pd(a)                  printf("%lf",a)
+#define pf(a)                   printf("%f",a)
+#define sd(a)                  scanf("%lf",&a)
+#define sf(a)                  scanf("%f",&a)
+#define si(a)                  scanf("%d",&a)
+#define sl(a)                  scanf("%lld",&a)
+#define sca(a)                 scanf("%s",a)
+#define loop(i,b)              for(i=0;i<b;i++)
+#define loope(i,b)             for(i=0;i<=b;i++)
+#define FOR(i,a,b)             for(i=a;i<b;i++)
+#define FORE(i,a,b)            for(i=a;i<=b;i++)
+#define rev(s)                 reverse(s.begin(),s.end())
+#define inf                    9000000000000000
+#define eps                    1e-9
+#define pie                    3.1415926536
+#define pb(a)                  push_back(a)
+#define pop(a)                pop_back(a)
+#define in(a,b)                insert({a,b})
+#define test                   printf(" <---I'm Here--> ")
+#define tc(a)                  while(a--)
+#define nl                     printf("\n")
+#define sorted(a)              sort(a.begin(),a.end())
+#define bsorted(a)             sort(a.begin(),a.end(),greater<ll>())
+#define arrsort(a,n)             sort(a, a + n)
+#define arrbsort(a)            sort(a, a + n, greater<ll>())
+#define i2s(arr,a)             std::sprintf(arr, "%d", a);
+#define tolow(a)               transform(a.begin(), a.end(), a.begin(), ::tolower)
+#define toup(a)               transform(a.begin(), a.end(), a.begin(), ::toupper)
+#define power(a,b)             (int)(pow(a,b)+eps)
+
+ll isStrictSorted(ll Arr[], ll n)
+{
+    ll i;
+    ll flag=1;
+    FORE(i,1,n-1)
+    {
+        if(Arr[i] < Arr[i+1])
+            flag = 1;
+        else
+        {
+            flag = 0;
+            return flag;
+        }  
+    }
+    return flag;
+    
+}
+
+int main()
+{
+    ll t;
+    cin>>t;
+    tc(t)
+    {
+        ll i,n;
+        ll count=0,flag = 0;
+        cin>>n;
+        ll arr[n];
+        FOR(i,0,n)
+            cin>>arr[i];
+
+
+        for(ll i=n-2; i>=0; i--)
+        {
+            while(arr[i]>=arr[i+1] && arr[i] > 0)
+            {
+                arr[i] /= 2;
+                count++;
+            }
+            if(arr[i] == arr[i+1]){
+                flag = 1;
+                cout<<"-1"<<endl;
+                break;
+            }
+        }
+        if(flag == 0)
+            cout<<count<<endl;     
+    }
+
+    return 0;
+}
